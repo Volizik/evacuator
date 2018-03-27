10214366fee72716bd28ea17e98a0c0801ca7c71
kavalar <apuc06@mail.ru>
2018-03-27 12:16:35 +0300

    end

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,42 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+(function ($) {
+    $(() => {
+        $('.gallery__slider').slick({
+            centerPadding: '60px',
+            slidesToShow: 2,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        arrows: true,
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        centerPadding: '120px',
+                        arrows: true,
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+        $('.questions__answer').each(function (i) {
+            if (i !== 0) {
+                $(this).hide(0);
+            }
+        });
 
-$(() => {
-	svg4everybody();
-});
+        $(document).on('click', '.questions__question', function (e) {
+            e.preventDefault();
+            const tabId = $(this).attr('href');
+            $('.questions__question').removeClass('questions__question--active');
+            $(this).addClass('questions__question--active');
+            $('.questions__answer').hide(0);
+            $(tabId).show();
+        });
+
+    });
+
+})(jQuery);
+
